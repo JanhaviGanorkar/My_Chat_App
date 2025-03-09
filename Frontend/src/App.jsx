fe9cf14e46a12857854e0f9c401ab9eee9d470f8
Janahvi <janhaviganorkar12@gmail.com>
2025-03-09 16:00:15 +0530

    Logout added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Register from "../src/auth/Register";
 import Login from "../src/auth/Login";
 import Home from "./pages/Home";
 import { useAuthStore } from "./store/authStore"; // Zustand Store
+import Logout from "../src/auth/Logout"
 
 // Protected Route Component
 function PrivateRoute({ element }) {
@@ -18,6 +19,7 @@ function App() {
         <Route path="/" element={<PrivateRoute element={<Home />} />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
       </Routes>
     </Router>
   );
